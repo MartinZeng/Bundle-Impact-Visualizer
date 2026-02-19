5d8e36547e379f0de21257ae2f7bea7b609d3f60
Mara <mara07476@gmail.com>
2026-02-19 10:44:52 -0500

    Updated changes to the CLI

--- src/server/cli.ts
+++ src/server/cli.ts
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import { getRemotePackageSize } from './analyzer.js';
 import picocolors from 'picocolors';
 
@@ -13,8 +12,6 @@ async function run() {
     process.exit(1);
   }
 
-  // Local mode: test our package.json reader
-  // Inside src/server/cli.ts
 
   if (packageName === '--local') {
     try {
@@ -24,27 +21,21 @@ async function run() {
 
       const deps = reader.extractDependencies(pkg);
 console.log('\n')
-       console.log(picocolors.gray('== ') +
-      //  picocolors.bold(picocolors.cyan('📦 B I V')) + 
-      // console.log(picocolors.white('') +
-       picocolors.bold(picocolors.cyan('📦Bundle Impact Visualizer')) + 
-      //  picocolors.white(''));
-      picocolors.gray(' =='));
-      // console.log(picocolors.gray('--------------------------------'))
-      // console.log('\n') 
-      console.log(
-  picocolors.cyan('\n✅ Local ') + 
-  picocolors.yellow('{}') + 
-  picocolors.italic(picocolors.cyan('package.json')) + 
-  picocolors.cyan(' loaded!')
-);
-      console.log(`${picocolors.gray('================================')}\n`);
-      console.log(`${picocolors.magenta('Name:')}    ${pkg.name}`);
-      console.log(`${picocolors.gray('--------------------------------')}\n`);
-      console.log(`${picocolors.magenta('Version:')} ${pkg.version}`);
-      console.log(`${picocolors.gray('--------------------------------')}\n`);
-      console.log(`${picocolors.magenta('Packages:')} ${Object.keys(deps).length}`);
-      console.log(`${picocolors.gray('--------------------------------')}\n`)
+     
+console.log(
+      picocolors.cyan('\n✅ Local ') +
+      picocolors.yellow('{}') +
+      picocolors.bold(picocolors.cyan(' package.json')) +
+      picocolors.cyan(' loaded') + picocolors.yellow('!')
+    );
+    console.log(`${picocolors.white('--------------------------------')}\n`);
+     console.log(`${picocolors.magenta('Name')}${picocolors.cyan(':')}     ${picocolors.blue(pkg.name)}`);
+    console.log(`${picocolors.white('--------------------------------')}\n`);
+    console.log(`${picocolors.magenta('Version')}${picocolors.cyan(':')}  ${picocolors.blue(pkg.version)}`);
+    console.log(`${picocolors.white('--------------------------------')}\n`);
+    console.log(`${picocolors.magenta('Packages')}${picocolors.cyan(':')} ${picocolors.blue(Object.keys(deps).length)}`);
+    console.log(`${picocolors.white('--------------------------------')}`);
+  
     } catch (err: any) {
       console.log(picocolors.red(`Local mode error: ${err.message}`));
       process.exit(1);
@@ -52,7 +43,7 @@ console.log('\n')
     return;
   }
    console.log(`\n🔎 ${picocolors.cyan(picocolors.bold('Searching for'))}...`);
-console.log(`${picocolors.gray('===================')}`);
+console.log(`${picocolors.white('----------------------')}`);
   
    
 const stats = await getRemotePackageSize(packageName);
@@ -66,18 +57,18 @@ const stats = await getRemotePackageSize(packageName);
         ),
       );
     }
-    console.log(`\n${picocolors.bold(picocolors.magenta(stats.name.charAt(0).toUpperCase() + stats.name.slice(1)))}`);
+    console.log(`${(picocolors.magenta(stats.name.charAt(0).toUpperCase() + stats.name.slice(1)))}`);
 
-    console.log(`${picocolors.gray('-------------------')}`);
+    console.log(`${picocolors.white('----------------------')}\n`);
    
     console.log(
-      `Minified: ${picocolors.yellow((stats.size / 1024).toFixed(2) + 'KB\⬆')}`,
-    );
-    console.log(`${picocolors.gray('-------------------')}`);
-    console.log(
-      `Gzipped:  ${picocolors.green((stats.gzip / 1024).toFixed(2) +   'KB\⬇')}`,
-    );
-    console.log(`${picocolors.gray('+++++++++++++++++++')}\n`);
+`${picocolors.blue('Minified')}: ${picocolors.yellow((stats.size / 1024).toFixed(2) + 'KB\⬆')}`
+   );
+   console.log(`${picocolors.white('----------------------')}\n`);
+   console.log(
+     `${picocolors.blue( 'Gzipped')}:  ${picocolors.green((stats.gzip / 1024).toFixed(2) + 'KB\⬇')}`,
+   );
+    console.log(`${picocolors.white('----------------------')}`);
   } else {
     console.log(picocolors.red(`could not find package: ${packageName}`));
   }
